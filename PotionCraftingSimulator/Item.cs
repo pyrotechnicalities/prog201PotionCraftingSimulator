c8892a7d63788bd5ad9c8020beb6b9272e5750ec
Leo Richnofsky <lostinhyrule18@gmail.com>
2024-03-18 13:08:11 -0500

    Add project files.

---
+++ PotionCraftingSimulator/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotionCraftingSimulator
+{
+    internal class Item
+    {
+        // generalized craft algorithm 
+        public Item CraftItem(Recipe recipe)
+        {
+            //if all the required elements for the recipe are in the player's inventory
+            // AND the amounts of those elements are equal to or greater than what is specified in the recipe
+            // then remove the ingredient items from the player's inventory or modify the amounts of the items
+            // and return an instance of the Item class that matches the recipe
+        }
+    }
+}
---
+++ PotionCraftingSimulator/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PotionCraftingSimulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // For next time: work on implimenting basic button functionality with code
+        // Also work on basic crafting algorithm 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
